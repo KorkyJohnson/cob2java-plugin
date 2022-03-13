af99ae78470881b3c3be57cf69b36a8c27108caf
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2022-03-13 15:59:58 -0400

    initial commit

---
+++ src/main/java/com/korky/GreetingMojo.java
@@ -0,0 +1,26 @@
+package com.hanna;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+ 
+/**
+ * Says "Hi" to the user.
+ *
+ */
+@Mojo( name = "sayhi")
+
+public class GreetingMojo extends AbstractMojo
+{
+    /**
+     * The greeting to display.
+     */
+    @Parameter( property = "sayhi.greeting", defaultValue = "Hello Default!" )
+    private String greeting;
+
+    public void execute() throws MojoExecutionException
+    {
+        getLog().info( greeting );
+    }
+}
