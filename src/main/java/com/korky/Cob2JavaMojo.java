f2aa57a1ab05ab786a6f16d5b1dec6bf0a978dbb
KorkyJ <88301804+KorkyJ@users.noreply.github.com>
2022-03-13 19:44:06 -0400

    basic file io

--- src/main/java/com/korky/Cob2JavaMojo.java
+++ src/main/java/com/korky/Cob2JavaMojo.java
@@ -1,5 +1,9 @@
 package com.korky;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -19,8 +23,40 @@ public class Cob2JavaMojo extends AbstractMojo
     @Parameter( property = "c2j.greeting", defaultValue = "Hello C2J!" )
     private String greeting;
 
+    public String infile = "input.txt";
+    public String outfile = "output.txt";
+
     public void execute() throws MojoExecutionException
     {
         getLog().info( greeting );
+        try {
+            CopyFile(infile,outfile);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            getLog().error("CopyFile() failed");
+            e.printStackTrace();
+        }
     }
+
+    public void CopyFile (String infile, String outfile) throws IOException {  
+        FileInputStream in = null;
+        FileOutputStream out = null;
+  
+        try {
+           in = new FileInputStream("C:\\proj\\cob2java\\src\\main\\resources\\.cbl\\input.cbl");
+           out = new FileOutputStream("C:\\proj\\cob2java\\src\\main\\resources\\.java\\input.java");
+           
+           int c;
+           while ((c = in.read()) != -1) {
+              out.write(c);
+           }
+        }finally {
+           if (in != null) {
+              in.close();
+           }
+           if (out != null) {
+              out.close();
+           }
+        }
+     }
 }
---
+++ src/main/resources/.cbl/input.cbl
@@ -0,0 +1,8 @@
+This is a COBOL file
+
+blahbalh
+
+more blah
+
+
+I hate Lukas' youtube channel
